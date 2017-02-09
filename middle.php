e6d452e7646219b2cb742232201f7c6291ff68ca
ylberpopaj <ylberpopaj@gmail.com>
2017-02-09 18:18:45 -0500

    Add files via upload

---
+++ middle.php
@@ -0,0 +1,39 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://web.njit.edu/~gg99/cs490/back.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+
+
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+	    "username=".$_POST['username']."&password=".$_POST['password']);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+
+
+echo "Output: ". $server_output;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://www.njit.edu/cp/login.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+
+
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+	    "username=".$_POST[‘cplogin’].”&password=".$_POST[‘userid’]);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+
+
+echo "Output: ". $server_output;
+
+?>
