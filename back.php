7b52b8ef9325d11e0d1d4147913044f1cf6743c9
Garry Guan <gg99@njit.edu>
2017-02-09 17:31:37 -0500

    Found out you can only connect to sql.njit from afs...

---
+++ back.php
@@ -0,0 +1,34 @@
+
+<?php
+	require 'db.php';
+
+	$db = new DB;
+
+	if(array_key_exists('username', $_POST)
+	{
+		$username = $_POST['username'];
+	}
+	if(array_key_exists('password', $_POST)
+	{
+		$pw 	  = $_POST['password'];
+	}
+	
+	if($username == "" || $pw == "")
+	{
+		echo "Username or pw not given";
+	}
+
+	// $username = "test";
+	// $pw = "test";
+
+	$result = $db->exec_query($username, $pw);
+
+	if($result->num_rows > 0)
+	{
+		echo "Success";
+	}
+	else
+	{
+		echo "Could not find match";
+	}
+?>
