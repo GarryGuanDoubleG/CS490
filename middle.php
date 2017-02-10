a7cc528265a7d5f405b77438ee895f361754ecd2
ylberpopaj <ylberpopaj@gmail.com>
2017-02-10 11:01:26 -0500

    Update middle.php

--- middle.php
+++ middle.php
@@ -13,21 +13,30 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec ($ch);
 
 curl_close ($ch);
-echo $server_output;
+if (strpos($server_output, 'Success') !== false) {
+    echo 'y';
+} else { echo 'n';}
+//echo $server_output;
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"https://www.njit.edu/cp/login.php");
+curl_setopt($ch, CURLOPT_URL,"https://cp4.njit.edu/cp/home/login");
 curl_setopt($ch, CURLOPT_POST, 1);
 
 
 curl_setopt($ch, CURLOPT_POSTFIELDS,
-	    "userid=".$_POST['username']."&cplogin=".$_POST['password']);
+	    "user=".$_POST['username']."&pass=".$_POST['password']."&uuid="."0xACA021");
+//curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.text');
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
          
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
 $server_output = curl_exec ($ch);
 curl_close ($ch);
-echo var_dump($server_output);
+if (strpos($server_output, 'Failed') !== false) {
+    echo 'n';
+} else { echo 'y';}
+//echo $server_output
+//echo var_dump($server_output);
 
 
 
